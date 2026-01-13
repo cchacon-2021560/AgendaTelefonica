c347ea338d3f51794020c92ae4508a7aaf9e6552
cchacon-2021560 <cchacon-2021560@kinal.edu.gt>
2026-01-12 18:23:28 -0600

    Mostramos los datos en perfil.html

---
+++ js/perfil.js
@@ -0,0 +1,8 @@
+const usuario = obtenerDatos("usuario");
+
+if (usuario) {
+    document.getElementById("perfil-correo").textContent = usuario.correo;
+    document.getElementById("perfil-password").textContent = usuario.password;
+} else {
+    window.location.href = "../index.html";
+}
