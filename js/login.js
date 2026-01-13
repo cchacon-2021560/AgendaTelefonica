a71b32ab0097b00e73a294fa72ad082f44d8da69
cchacon-2021560 <cchacon-2021560@kinal.edu.gt>
2026-01-12 18:08:46 -0600

    Preparar estructuras iniciales de contactos y tareas

---
+++ js/login.js
@@ -0,0 +1,29 @@
+//
+const formLogin = document.getElementById("form-login");
+
+formLogin.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const correo = document.getElementById("correo").value;
+    const password = document.getElementById("password").value;
+
+    const usuario = {
+        correo: correo,
+        password: password
+    };
+
+    guardarDatos("usuario", usuario);
+
+    //inicializo
+    if(!obtenerDatos("contactos")) {
+        guardarDatos("contactos", []);
+    }
+
+    if(!obtenerDatos("tareas")) {
+        guardarDatos("tareas", []);
+    }
+    
+
+    //cambiar a contacto.html*
+    window.location.href="vistas/perfil.html";
+})
