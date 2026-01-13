e342481af5ab9708bd387a4796c3ba94e8a2ae3f
cchacon-2021560 <cchacon-2021560@kinal.edu.gt>
2026-01-13 14:34:03 -0600

    Crear la renderización de los contactos en js

--- js/contacto.js
+++ js/contacto.js
@@ -1,4 +1,3 @@
-
 const usuario = obtenerDatos("usuario");
 if (!usuario) {
     window.location.href = "../index.html";
@@ -19,3 +18,60 @@ btnPerfil.addEventListener("click", () => {
 cerrarPerfil.addEventListener("click", () => {
     modalPerfil.classList.remove("activo");
 });
+
+//CONTACTOS:
+const contactos = [
+    {
+        id: 1,
+        nombre: "Juan Pérez",
+        telefono: "5555-1234",
+        correo: "juan@gmail.com",
+        favorito: false
+    },
+    {
+        id: 2,
+        nombre: "María López",
+        telefono: "5555-5678",
+        correo: "maria@gmail.com",
+        favorito: true
+    },
+    {
+        id: 3,
+        nombre: "Carlos Gómez",
+        telefono: "5555-9012",
+        correo: "carlos@gmail.com",
+        favorito: false
+    }
+];
+
+const listaContacto=document.querySelector(".contactos-lista");
+
+function renderizarContactos(lista) {
+    listaContactos.innerHTML = "";
+    lista.forEach(contacto => {
+        const card = document.createElement("article");
+        card.classList.add("contacto-card");
+
+        card.innerHTML = `
+            <div class="contacto-info">
+                <h3>${contacto.nombre}</h3>
+                <p>${contacto.telefono}</p>
+                <p>${contacto.correo}</p>
+            </div>
+
+            <div class="contacto-acciones">
+                <button class="btn-icono favorito" title="Favorito">
+                    <i class="fa-solid fa-heart"></i>
+                </button>
+
+                <button class="btn-icono eliminar" title="Eliminar">
+                    <i class="fa-solid fa-trash"></i>
+                </button>
+            </div>
+        `;
+
+        listaContactos.appendChild(card);
+    });
+}
+
+renderizarContactos(contactos);
