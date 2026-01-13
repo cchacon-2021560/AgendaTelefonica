5c0cd1a29fcc2e0a6372eaf1691721e46cf89a6c
cchacon-2021560 <cchacon-2021560@kinal.edu.gt>
2026-01-12 18:42:34 -0600

    Borrar pefil para reemplazarlo como modal en contactos

--- js/perfil.js
+++
@@ -1,9 +0,0 @@
-//llammamos a la funcion como objeto
-const usuario = obtenerDatos("usuario");
-
-if (usuario) {
-    document.getElementById("perfil-correo").textContent = usuario.correo;
-    document.getElementById("perfil-password").textContent = usuario.password;
-} else {
-    window.location.href = "../index.html";
-}
